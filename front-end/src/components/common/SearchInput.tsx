974e6566210df783c8127e284f5e9fc2244dd1f0
andrejusby <andriusby@gmail.com>
2024-11-17 09:25:11 +0200

    add-searchInput

--- front-end/src/components/common/SearchInput.tsx
+++ front-end/src/components/common/SearchInput.tsx
@@ -1,11 +1,11 @@
-import styles from './SearchInput.module.scss'
+import styles from "./SearchInput.module.scss";
 
 interface SearchInputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const SearchInput = ({ ...props }: SearchInputProps) => {
   return (
-    <input className={styles.searchInput} placeholder='Search' {...props} />
-  )
-}
+    <input className={styles.searchInput} placeholder="Search" {...props} />
+  );
+};
 
-export default SearchInput
+export default SearchInput;
