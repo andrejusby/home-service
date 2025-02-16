a2a52e4c73ca752deea2419a4f326a028207ae4c
andrejusby <andriusby@gmail.com>
2025-02-16 10:15:33 +0200

    refactor-MobileCategoryList-avatar-scss

--- front-end/src/components/category/MobileCategoryList.tsx
+++ front-end/src/components/category/MobileCategoryList.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useCategories } from "./hooks";
 import styles from "./MobileCategoryList.module.scss";
@@ -6,27 +5,18 @@ import styles from "./MobileCategoryList.module.scss";
 const MobileCategoryList = () => {
   const { data: categories } = useCategories();
   const navigate = useNavigate();
-  const { category } = useParams(); 
-  const [selectedCategory, setSelectedCategory] = useState("");
-
-  useEffect(() => {
-    if (category) {
-      setSelectedCategory(category);
-    }
-  }, [category]);
+  const { category } = useParams();
 
   const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedValue = event.target.value;
-    setSelectedCategory(selectedValue);
     if (selectedValue) {
       navigate(`/search/${selectedValue}`);
     }
   };
 
   return (
-      
     <div className={styles.mobileDropdown}>
-      <select onChange={handleChange} value={selectedCategory}>
+      <select onChange={handleChange} value={category || ""}>
         <option value="">Select Category</option>
         {categories?.map((category) => (
           <option key={category.name} value={category.name}>
